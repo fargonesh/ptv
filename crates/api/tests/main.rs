5a00e6bb155e4591219ececb09db0973d079cbc8
Ivy Pierlot <ivyp@outlook.com.au>
2025-12-04 20:04:35 +1100

    fix tests

--- crates/api/tests/main.rs
+++ crates/api/tests/main.rs
@@ -24,6 +24,7 @@ pub mod test {
             Client::new(std::env::var(DEVID).unwrap(), std::env::var(KEY).unwrap())
         }
     });
+    static NOW: Lazy<chrono::NaiveDateTime> = Lazy::new(|| chrono::Utc::now().naive_utc());
 
     // TODO: Find sensible constants
     static ROUTE_TYPE: RouteType = RouteType::Train; // Train
@@ -40,7 +41,7 @@ pub mod test {
             "{}",
             serde_json::to_string_pretty(&DisruptionMode::MetroTrain).unwrap()
         );
-        make_test!(map, get_departures_by_route_type_and_stop_id, GetDeparturesByRouteTypeAndStopIdParams => [ gtfs, include_cancelled], ROUTE_TYPE, STOP_ID );
+        make_test!(map, get_departures_by_route_type_and_stop_id, GetDeparturesByRouteTypeAndStopIdParams => [ gtfs, include_cancelled, date_utc: DateTime::Naive(*NOW)], ROUTE_TYPE, STOP_ID );
         make_test!(map, get_departures_by_route_type_and_stop_id_and_route_id, GetDeparturesByRouteTypeAndStopIdAndRouteIdParams => [ gtfs, include_cancelled], ROUTE_TYPE, STOP_ID, ROUTE_ID );
         make_test!(map, get_directions_by_direction_id, DIRECTION_ID);
         make_test!(
@@ -51,8 +52,8 @@ pub mod test {
         );
         make_test!(map, get_directions_by_route_id, ROUTE_ID);
         make_test!(map, get_disruption_by_disruption_id, DisruptionId(1));
-        make_test!(map, get_disruptions, GetDisruptionsParams => [ route_types: vec![RouteType::Train] ]);
-        make_test!(map, get_disruptions_by_route_id, GetDisruptionsByRouteIdParams => [ disruption_status: DisruptionStatus::Planned ], ROUTE_ID);
+        make_test!(map, get_disruptions, GetDisruptionsParams => [ route_types: vec![RouteType::Train], disruption_status: DisruptionStatus::Planned, disruption_status: DisruptionStatus::Current, disruption_modes: Modes(DisruptionMode::MetroTrain) ]);
+        make_test!(map, get_disruptions_by_route_id, GetDisruptionsByRouteIdParams => [ disruption_status: DisruptionStatus::Planned, disruption_status: DisruptionStatus::Current ], ROUTE_ID);
         make_test!(map, get_disruptions_by_route_id_and_stop_id, GetDisruptionsByRouteIdAndStopIdParams => [ disruption_status: DisruptionStatus::Planned ], ROUTE_ID, STOP_ID);
         make_test!(map, get_disruptions_by_stop_id, GetDisruptionsByStopIdParams => [ disruption_status: DisruptionStatus::Planned ], STOP_ID);
         make_test!(map, get_fare_estimate_by_min_zone_and_max_zone,GetFareEstimateByMinZoneAndMaxZoneParams => [ is_journey_in_free_tram_zone ], 1, 2);
@@ -64,7 +65,7 @@ pub mod test {
         make_test!(map, get_runs_by_route_id, GetRunsByRouteIdParams => [ expand: vec![ExpandOptions::Direction] ], ROUTE_ID);
         make_test!(map, get_runs_by_route_id_and_route_type, GetRunsByRouteIdAndRouteTypeParams => [ expand: vec![ExpandOptions::Direction] ], ROUTE_ID, ROUTE_TYPE);
         make_test!(map, get_runs_by_run_ref, GetRunsByRunRefParams => [ expand: vec![ExpandOptions::All] ], RUN_REF);
-        make_test!(map, get_search_result_by_search_term, GetSearchResultBySearchTermParams => [ include_addresses, route_types: vec![RouteType::Train] ], "Flinders Street");
+        make_test!(map, get_search_result_by_search_term, GetSearchResultBySearchTermParams => [ include_addresses, route_types: vec![RouteType::Train], route_types: vec![RouteType::Bus] ], "Flinders Street");
         make_test!(map, get_stop_by_stop_id_and_route_type, GetStopByStopIdAndRouteTypeParams => [ stop_location, stop_amenities  ], STOP_ID, ROUTE_TYPE);
         make_test!(map, get_stopping_pattern_by_run_ref_and_route_type, GetStoppingPatternByRunRefAndRouteTypeParams => [ expand: vec![ExpandOptions::All] ], RUN_REF, ROUTE_TYPE);
         make_test!(map, get_stops_by_distance_by_latitude_and_longitude, GetStopsByDistanceByLatitudeAndLongitudeParams => [ max_results: 20, max_distance: 30.0 ], -37.8100, 144.9620);
